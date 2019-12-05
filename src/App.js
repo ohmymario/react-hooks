928bd3e38b23e3edc16d91f38b2880089e91130c
Mario <mariob360@gmail.com>
2019-12-04 16:13:22 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-
-import Test from './Test';
 import TodoApp from './TodoApp';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Test/> */}
       <TodoApp/>
     </div>
   );
 }
 
 export default App;
-
-// - TodoApp
-//   - TodoForm
-//   - TodoList
-//     - TodoItem
